2476deb5d1773e83ab8b128c1ca6a367f8bb307a
simonallan <simon.allan@cancer.org.uk>
2021-08-25 15:36:24 +0100

    context switching

--- dynatrace-transitvpc/lib/targetvpc-stack.ts
+++ dynatrace-transitvpc/lib/targetvpc-stack.ts
@@ -1,5 +1,6 @@
 import * as cdk from '@aws-cdk/core';
 import * as ec2 from '@aws-cdk/aws-ec2'
+import { setFlagsFromString } from 'v8';
 
 export class TargetvpcStack extends cdk.Stack {
   constructor(scope: cdk.Construct, id: string, props?: cdk.StackProps) {
@@ -9,7 +10,7 @@ export class TargetvpcStack extends cdk.Stack {
     const targetvpc_id = "vpc-17f02973"
 
     // Dynamically returns target VPC info
-    const targetvpc = ec2.Vpc.fromLookup(this, 'targetvpc', {
+    this.vpc_id = ec2.Vpc.fromLookup(this, 'targetvpc', {
         vpcId: targetvpc_id
     })
   }
